ae19e1946a3cb0a9fefbcfd60b1a1b2759b01dc5
咸小鱼 <1307083930@qq.com>
2022-05-21 17:07:07 +0800

    初始化项目

---
+++ src/main/java/com/cqut/livechat/LiveChatApplication.java
@@ -0,0 +1,17 @@
+package com.cqut.livechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Augenstern
+ * @date 2022/5/21
+ */
+@SpringBootApplication
+public class LiveChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveChatApplication.class, args);
+    }
+
+}
